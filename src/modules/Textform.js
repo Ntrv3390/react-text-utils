ad6976b9242a34407a1087067e330bf07c738dbd
Ntrv3390 <78497133+Ntrv3390@users.noreply.github.com>
2023-07-06 14:41:47 +0530

    Uppercase Lowercase Logic

---
+++ src/modules/Textform.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+
+export const Textform = (props) => {
+    const handleUpperCase = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+    const handleLowerCase = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    const handleChange = (event) => {
+        setText(event.target.value);
+    }
+    const [text, setText] = useState('Enter text to get started');
+  return (
+    <div className="container mt-3">
+        <h1>{props.heading}</h1>
+        <textarea className="form-control" value={text} rows={12} onChange={handleChange}></textarea>
+        <span>
+            <button className="btn btn-danger mt-2" onClick={handleUpperCase}>Convert to uppercase</button>
+            <button className="btn btn-warning mt-2 mx-2" onClick={handleLowerCase}>Convert to lowercase</button>
+        </span>
+        
+    </div>
+  )
+}
